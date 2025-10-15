92b4d17c5afc9d416e898d7b83a3e9ddcf1426de
d00262695 <d00262695@dkit.ie>
2025-10-15 23:21:49 +0100

    docs: add JavaDoc for getPos() method

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -1,8 +1,13 @@
 package utils;
 
 public class NumberUtils {
-
-    public static int getPost(int[] arr, int target) {
+/**
+ * finds the first pos of a traget number in an array
+ * @param arr the array to search
+ * @param target the number to find
+ * @return the index of the first pos, or -1 if not found
+**/
+    public static int getPos(int[] arr, int target) {
         for (int i =0; i < arr.length; i++) {
             if (arr [i] == target) return i;
             }
