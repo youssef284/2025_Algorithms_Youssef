3a0e1092a0c415f999cd126ff9aeaadcc131aaa0
d00262695 <d00262695@dkit.ie>
2025-10-15 23:43:58 +0100

    feat: add getLastPos() method to find last position in array

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -1,16 +1,26 @@
 package utils;
 
 public class NumberUtils {
-/**
- * finds the first pos of a traget number in an array
- * @param arr the array to search
- * @param target the number to find
- * @return the index of the first pos, or -1 if not found
-**/
+    /**
+     * finds the first pos of a traget number in an array
+     *
+     * @param arr    the array to search
+     * @param target the number to find
+     * @return the index of the first pos, or -1 if not found
+     **/
     public static int getPos(int[] arr, int target) {
-        for (int i =0; i < arr.length; i++) {
-            if (arr [i] == target) return i;
-            }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
         return -1;
     }
+
+    public static int getLastPos(int[] arr, int target) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+
 }
+
