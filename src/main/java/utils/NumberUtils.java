98d477cc301d3002891eda26a1e168fda10733fc
d00262695 <d00262695@dkit.ie>
2025-10-16 20:58:11 +0100

    feat: add getMax() method to find largest number in array

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -104,6 +104,16 @@ public class NumberUtils {
             return true; // all elements from A found in B
         }
 
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
 }
 
 
